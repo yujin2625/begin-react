fb570f0e90f82fd0e3c19250f23f5eefbf285967
Yujin Park <yujin2625@gmail.com>
2024-09-03 13:31:02 +0900

    dd

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useRef } from "react";
 import Hello from "./Hello";
 import "./App.css";
 import Wrapper from "./Wrapper";
 import Counter from "./Counter";
 import InputSample from "./InputSample";
 import UseStateTest from "./UseStateTest";
+import UserList from "./UserList";
 
 function App() {
-  return <InputSample />;
+  const users = [
+    {
+      id: 1,
+      username: "velopert",
+      email: "public.velopert@gmail.com",
+    },
+    {
+      id: 2,
+      username: "tester",
+      email: "tester@example.com",
+    },
+    {
+      id: 3,
+      username: "liz",
+      email: "liz@example.com",
+    },
+  ];
+
+  const nextId = useRef(4);
+  const onCreate = () => {
+    nextId.current += 1;
+  };
+  return <UserList users={users} />;
 }
 
 export default App;
--- src/UserList.js
+++ src/UserList.js
@@ -1,21 +1,29 @@
 import React from "react";
 
-function UserList() {
-  const users = [
-    {
-      id: 1,
-      name: "Alice",
-      email: "alice@example.com",
-    },
-    {
-      id: 2,
-      name: "Bob",
-      email: "bob@example.com",
-    },
-    {
-      id: 3,
-      name: "Charlie",
-      email: "charlie@example.com",
-    },
-  ];
+function User({ user }) {
+  return (
+    <div>
+      <b>{user.username}</b>
+      <span>({user.email})</span>
+    </div>
+  );
 }
+
+function UserList({ users }) {
+  return (
+    <>
+      {users.map((user) => (
+        <User user={user} key={user.id} />
+      ))}
+
+      {/* {users.map((user, index) => (
+        <User user={user} key={index} />
+      ))} */}
+      <User user={users[0]} />
+      <User user={users[1]} />
+      <User user={users[2]} />
+    </>
+  );
+}
+
+export default UserList;
